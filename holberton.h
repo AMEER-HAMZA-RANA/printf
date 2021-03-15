296c5074b438336e9926ae79eda52350d9f1c1a4
travis1999 <trevoroguna@gmail.com>
2021-03-15 11:44:25 +0300

    function declarations

---
+++ holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void _print_str(char *string);
+void _print_int(int var);
+void _print_double(double var);
+int _printf(const char *format, ...);
+
+#endif
