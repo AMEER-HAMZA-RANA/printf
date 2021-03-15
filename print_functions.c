b493192de600d8a7a9085aae2f264942d00c8a41
travis1999 <trevoroguna@gmail.com>
2021-03-15 11:45:18 +0300

    added implementations for _print_str and _print_int

---
+++ print_functions.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+
+/**
+ * _print_str - prints a string
+ * @string: string to be printed
+ */
+ 
+void _print_str(char *string)
+{
+	while (*string)
+		_putchar(*string++);
+	
+}
+
+/**
+ * _print_int - prints an integer
+ * @var: variable to be printed
+ */
+
+void _print_int(int var)
+{
+	if (var < 0)
+	{
+		_putchar('-');
+		var = -var;
+	}
+	
+	if (var / 10)
+		_print_int(var / 10);
+
+	_putchar(var % 10 + '0');
+}
+
+/**
+ * _print_float - prints a floating point number
+ * @var: float to be printed
+ */
+
+void _print_float(float)
+{
+
+} 
