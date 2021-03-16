477c6d1cb302cd5ca5cdadcd5cd077f31891c70c
travis1999 <trevoroguna@gmail.com>
2021-03-16 12:09:52 +0300

    fixed issues causing segmentation faults

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include <stdarg.h>
-#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -8,39 +7,61 @@
   * Return: number of characters printed(
   * excluding the null terminator)
   */
+
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0;
-	const char *ptr;
-	/* char c; */
-	char *str;
+	int count;
+	char arg_c;
+	int arg_i;
+	char *arg_str;
 	va_list args;
 
 	va_start(args, format);
-	ptr = format;
 
-	for (; *ptr != '\0'; ptr++, i++)
+	for (count = 0; *(format + count) != '\0'; count++)
 	{
-		if (*ptr != '%')
+		if (format[count] == '%')
 		{
-			_putchar(*ptr);
-			continue;
+			++count;
+
+			switch(format[count])
+			{
+				case 'c':
+					arg_c = va_arg(args, int);
+					_putchar(arg_c);
+					break;
+
+				case 's':
+					arg_str = va_arg(args, char *);
+					_print_str(arg_str);
+					break;
+
+				case '%':
+					_putchar('%');
+					break;
+
+				case 'd':
+					arg_i = va_arg(args, int);
+					_print_int(arg_i);
+					break;
+
+				case 'i':
+					arg_i = va_arg(args, int);
+					_print_int(arg_i);
+					break;
+
+				default:
+					_putchar('%');
+					_putchar(format[count]);
+			}
 		}
-		switch (*++ptr)
+
+		else
 		{
-			case 'c':
-				/* c = va_arg(args, int); */
-				_putchar(va_arg(args, int));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				_print_str(str);
-				break;
-			case '%':
-				_putchar('%');
-				break;
+			_putchar(format[count]);	
 		}
 	}
+
 	va_end(args);
-	return (i);
+	return (count);
 }
