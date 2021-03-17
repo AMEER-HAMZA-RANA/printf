c58d07ba3073caf2523bcba9af1becc7fd109895
Erick Kiminza <kiminzaeric@gmail.com>
2021-03-17 20:01:02 +0300

    update for binary conversion

--- conversions.c
+++ conversions.c
@@ -5,17 +5,28 @@
   * @n: number passed for convertion.
   * Return: binary number.
   */
-long to_Binary(int n)
+int to_Binary(unsigned int n)
 {
-	long binary = 0;
-	int modula, tmp = 1;
-
-	while (n != 0)
+	int binary[sizeof(unsigned int) * 8], modula, j,
+	    chars_counter = 0;
+	if (n == 0)
 	{
-		modula = n % 2;
-		n /= 2;
-		binary = binary + modula * tmp;
-		tmp *= 10;
+		chars_counter += _putchar('0');
+		return (chars_counter);
 	}
-	return (binary);
+	if (n == 1)
+	{
+		chars_counter += _putchar('1');
+		return (chars_counter);
+	}
+	modula = 0;
+	while (n > 0)
+	{
+		binary[modula] = '0' + (n % 2);
+		n /= 2;
+		modula++;
+	}
+	for (j = modula - 1; j >= 0; j--)
+		chars_counter += _putchar(binary[j]);
+	return (chars_counter);
 }
